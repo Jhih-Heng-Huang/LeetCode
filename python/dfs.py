09f595ccf54488bf1455c2c5e672406206b60811
Jhih-Heng-Huang <kklive128@gmail.com>
2018-02-12 10:02:39 +0800

    <LeetCode> - python
    	- 743. Network Delay Time
    	- implement Dijkstra's algorithm
    	- use list() as a stack to implement depth-first-search
    
    reviewer: AlbertHuang

--- python/dfs.py
+++ python/dfs.py
@@ -1,3 +1,5 @@
+import sys
+
 class DepthFirstSearch(object):
 	def __init__(self):
 		pass
@@ -17,4 +19,58 @@ class DepthFirstSearch(object):
 			sum += self.SumPathNum(root.right,
 				val * 10 + root.right.val)
 		return sum
-			
+
+	# type times: List[List[]]
+	# type N: int
+	# rtype: dict{List[(target, weight)]}
+	def GetAdjacencyList(self, times, N):
+		(source , target, weight) = (0, 1, 2)
+		adj_list = dict()
+		for i in range(1, N+1):
+			adj_list[i] = list()
+		for edge in times:
+			adj_list[edge[source]].append((edge[target], edge[weight]))
+		return adj_list
+
+	# type adj_list: dict{List[(target, weight)]}
+	# type start_node: int
+	# rtype: List[]
+	def DijstraAlgo(self, adj_list, start_node):
+		dist = dict()
+		for key in adj_list.keys():
+			dist[key] = (sys.maxint)
+		dist[start_node] = 0
+		
+		# visit the start node then visit its neighbors
+		# then visit their neighbors ...and so no
+		# update the dist if the dist[key] > len
+		stack = [start_node]
+		while stack:
+			node = stack.pop()
+			for tar, wei in adj_list[node]:
+				new_dist = dist[node] + wei
+				if new_dist < dist[tar]:
+					dist[tar] = new_dist
+					stack.append(tar)
+		print dist
+		return dist
+
+	# :type times: List[List[int]]
+	# :type N: int
+	# :type K: int
+	# :rtype: int
+	def networkDelayTime(self, times, N, K):
+		# turn times to the adjacency list
+		adj_list = self.GetAdjacencyList(times, N)
+		print adj_list
+		print adj_list.values()
+		# find out the minimax network delay time
+		dist = self.DijstraAlgo(adj_list, K)
+		max_len = 0
+		for length in dist.values():
+			if length == sys.maxint:
+				return -1
+			elif length > max_len:
+				max_len = length
+		return max_len
+
--- python/main.py
+++ python/main.py
@@ -3,10 +3,5 @@ from tree import TreeNode
 
 if __name__ == '__main__':
 	sol = dfs()
-	root = TreeNode(5)
-	root.left = TreeNode(2)
-	root.right = TreeNode(3)
-	root.left.left = TreeNode(0)
-	root.right.left = TreeNode(0)
-	root.right.right = TreeNode(1)
-	print sol.sumNumbers(root)
+	times = [[1,2,3],[1,3,1],[3,4,5]]
+	print sol.networkDelayTime(times = times, N=4, K=3)
