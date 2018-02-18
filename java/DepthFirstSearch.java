e4f65b662e041a377630ad02694fe89225d2cee5
Jhih-Heng-Huang <kklive128@gmail.com>
2018-02-18 22:12:36 +0800

    <LeetCode> - java branch
    	- add a branch for java programming
    	- add two "hello world" java files
    
    reviewer: AlbertHuang

---
+++ java/DepthFirstSearch.java
@@ -0,0 +1,5 @@
+public class DepthFirstSearch {
+	public void function() {
+		System.out.println("Hello, DFS");
+	}
+}
---
+++ java/Main.java
@@ -0,0 +1,9 @@
+
+// import DepthFirstSearch;
+
+public class Main {
+	public static void main(String[] args) {
+		DepthFirstSearch dfs = new DepthFirstSearch();
+		dfs.function();
+	}
+}
