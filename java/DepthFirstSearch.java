1a3243e20de16d825e2f3a3a34fe22ade410a39e
Jhih-Heng-Huang <kklive128@gmail.com>
2018-02-19 19:44:44 +0800

    <LeetCode> - java
    	- 129. Sum Root to Leaf Numbers
    
    reviewer: AlbertHuang

--- java/DepthFirstSearch.java
+++ java/DepthFirstSearch.java
@@ -1,5 +1,25 @@
 public class DepthFirstSearch {
-	public void function() {
-		System.out.println("Hello, DFS");
+	public int sumNumbers(TreeNode root) {
+		if (root == null) {
+			return 0;
+		} else {
+			return SumNumbers(root, root.val);
+		}
+	}
+
+	private int SumNumbers(TreeNode root, int val) {
+		if (root.left == null && root.right == null) {
+			return val;
+		}
+		int sum_val = 0;
+		if (root.left != null) {
+			int left_val = val * 10 + root.left.val;
+			sum_val += SumNumbers(root.left, left_val);
+		}
+		if (root.right != null) {
+			int right_val = val * 10 + root.right.val;
+			sum_val += SumNumbers(root.right, right_val);
+		}
+		return sum_val;
 	}
 }
--- java/Main.java
+++ java/Main.java
@@ -1,9 +1,5 @@
-
-// import DepthFirstSearch;
-
 public class Main {
 	public static void main(String[] args) {
 		DepthFirstSearch dfs = new DepthFirstSearch();
-		dfs.function();
 	}
 }
