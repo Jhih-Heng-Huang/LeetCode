f1e88e9ae8ee51c270e089a423a8164ce5cb02c4
Jhih-Heng-Huang <kklive128@gmail.com>
2018-02-20 10:21:42 +0800

    <LeetCode> - java
    	- 129. Sum Root to Leaf Numbers
    	- use stack to implement DFS method
    	- modify the makefile
    
    reviewer: AlbertHuang

--- java/DepthFirstSearch.java
+++ java/DepthFirstSearch.java
@@ -1,25 +1,32 @@
+import java.util.Stack;
+import java.util.Map;
+import java.util.TreeMap;
+import java.lang.Integer;
+
 public class DepthFirstSearch {
 	public int sumNumbers(TreeNode root) {
 		if (root == null) {
 			return 0;
-		} else {
-			return SumNumbers(root, root.val);
 		}
-	}
+		// using stack and map to implement DFS
+		Stack<TreeNode> stack = new Stack<TreeNode>();
+		stack.push(root);
+		int sum = 0;
+		while (!stack.empty()) {
+			TreeNode node = stack.pop();
 
-	private int SumNumbers(TreeNode root, int val) {
-		if (root.left == null && root.right == null) {
-			return val;
+			if (node.left != null) {
+				node.left.val += node.val * 10;
+				stack.push(node.left);
+			}
+			if (node.right != null) {
+				node.right.val += node.val * 10;
+				stack.push(node.right);
+			}
+			if (node.left == null && node.right == null) {
+				sum += node.val;
+			}
 		}
-		int sum_val = 0;
-		if (root.left != null) {
-			int left_val = val * 10 + root.left.val;
-			sum_val += SumNumbers(root.left, left_val);
-		}
-		if (root.right != null) {
-			int right_val = val * 10 + root.right.val;
-			sum_val += SumNumbers(root.right, right_val);
-		}
-		return sum_val;
+		return sum;
 	}
 }
