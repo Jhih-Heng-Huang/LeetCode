a537a52ab89da78bfb31f4d09da9a721456d2b09
Jhih-Heng-Huang <kklive128@gmail.com>
2018-04-04 17:44:28 +0800

    <LeetCode> - 695, 547
    	- 695. Max Area of Island
    	- Algo: Depth-first-search
    	- when you encounter the element with 1,
    	count the area space with this element
    
    	- 547. Friend Circles
    	- Algo: Depth-First-Search
    	- Using DFS to search the friend circle from one unvisited person
    
    reviewer: AlbertHuang

--- cpp/lib/depth_first_search.cpp
+++ cpp/lib/depth_first_search.cpp
@@ -1,8 +1,12 @@
 #include "depth_first_search.h"
 #include <utility>
+#include <algorithm>
+#include <stack>
 
 using std::set;
 using std::pair;
+using std::max;
+using std::stack;
 
 // 200. Number of Islands
 int DepthFirstSearch::numIslands(const vector<vector<char>>& grid) {
@@ -63,6 +67,65 @@ void DepthFirstSearch::genIsland(const vector<vector<char>>& grid,
 		}
 		if (col + 1 < grid[0].size()) {
 			stack.push_back(point + 1);
+		}
+	}
+}
+
+// 695. Max Area of Island
+int DepthFirstSearch::maxAreaOfIsland(vector<vector<int>>& grid) {
+	int max_count = 0;
+	for (int row = 0; row < grid.size(); ++row) {
+		for (int col = 0; col < grid[row].size(); ++col) {
+			int position = row * grid[row].size() + col;
+			int count = this->getAreaOfIsland(grid, position);
+			max_count = max(max_count, count);
+		}
+	}
+	return max_count;
+}
+
+int DepthFirstSearch::getAreaOfIsland(vector<vector<int>>& grid, int position) {
+	int row = position/grid[0].size();
+	int col = position%grid[0].size();
+	if (grid[row][col] == 0) {
+		return 0;
+	}
+
+	grid[row][col] = 0;
+	int count = 1;
+	if (row - 1 >= 0) {
+		count += this->getAreaOfIsland(grid, position - grid[0].size());
+	}
+	if (row + 1 < grid.size()) {
+		count += this->getAreaOfIsland(grid, position + grid[0].size());
+	}
+	if (col - 1 >= 0) {
+		count += this->getAreaOfIsland(grid, position - 1);
+	}
+	if (col + 1 < grid[0].size()) {
+		count += this->getAreaOfIsland(grid, position + 1);
+	}
+	return count;
+}
+
+// 547. Friend Circles
+int DepthFirstSearch::findCircleNum(vector<vector<int>>& M) {
+	int count = 0;
+	for (int i = 0; i < M.size(); ++i) {
+		if (M[i][i] == 1) {
+			this->findCircle(M, i);
+			++count;
+		}
+	}
+	return count;
+}
+
+void DepthFirstSearch::findCircle(vector<vector<int>> &M, int person) {
+	for (int j = 0; j < M[person].size(); ++j) {
+		if (M[person][j] == 1) {
+			M[person][j] = 0;
+			M[j][person] = 0;
+			this->findCircle(M, j);
 		}
 	}
 }
--- cpp/lib/depth_first_search.h
+++ cpp/lib/depth_first_search.h
@@ -16,6 +16,18 @@ private:
 	bool isExistedIsland(const vector<set<int>>& islands, int point);
 	void genIsland(const vector<vector<char>>& grid,
 						vector<set<int>>& islands, int start_point);
+
+// 695. Max Area of Island
+public:
+	int maxAreaOfIsland(vector<vector<int>>& grid);
+private:
+	int getAreaOfIsland(vector<vector<int>>& grid, int position);
+
+// 547. Friend Circles
+public:
+	int findCircleNum(vector<vector<int>>& M);
+private:
+	void findCircle(vector<vector<int>> &M, int person);
 };
 
 #endif
