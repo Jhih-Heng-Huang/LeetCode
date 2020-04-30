d8ed33cfb0b2fbf693aa21fbf0ba7439d4a886ea
albert <albert@rayark.com>
2020-05-01 06:24:18 +0800

    Create an environment for developing csharp

---
+++ csharp/main.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace LeetCode
+{
+    public class main
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello C#.");
+        }
+    }
+}
