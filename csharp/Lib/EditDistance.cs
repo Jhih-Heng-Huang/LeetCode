af28c261da19c80ad60f0b733a29cc861af31438
albert <albert@rayark.com>
2020-05-03 21:41:17 +0800

    Add a library for the dynamic programming

---
+++ csharp/Lib/EditDistance.cs
@@ -0,0 +1,10 @@
+namespace LeetCode.DynamicProgramming
+{
+    public static class EditDistance
+    {
+        public static int GetMinDistance(string word1, string word2)
+        {
+            return 0;
+        }
+    }
+}
--- csharp/main.cs
+++ csharp/main.cs
@@ -2,11 +2,13 @@ using System;
 
 namespace LeetCode
 {
+    using DynamicProgramming;
     public class main
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello C#.");
+            Console.WriteLine("Edit Distance: " + EditDistance.GetMinDistance(string.Empty, string.Empty));
         }
     }
 }
