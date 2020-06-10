3db2a54fa19c8254d37356acaf97d9956fc58642
albert <albert@rayark.com>
2020-06-10 08:51:29 +0800

    Solve LeetCode 226. Invert Binary Tree in Kotlin

---
+++ kotlin/Lib/InvertBinaryTree.kt
@@ -0,0 +1,24 @@
+package LeetCode.InvertBinaryTree
+
+/*
+226. Invert Binary Tree
+Test cases:
+[4,2,7,1,3,6,9]
+ */
+
+import LeetCode.TreeNode
+
+fun invertTree(root: TreeNode?): TreeNode?
+{
+    if (root == null)
+        return null
+    
+    // invert left subtree & right subtree
+    val leftSubTree = invertTree(root.right)
+    val rightSubTree = invertTree(root.left)
+
+    var result: TreeNode? = TreeNode(root.`val`)
+    result!!.left = leftSubTree
+    result!!.right = rightSubTree
+    return result
+}
