c69acc5b5fc1fea28105d74c3ebf47b2aab942a7
Jhih-Heng-Huang <kklive128@gmail.com>
2020-10-02 06:36:36 +0800

    Solve LeetCode 264. Ugly Number II in Kotlin

---
+++ kotlin/Lib/264.UglyNumberII.kt
@@ -0,0 +1,27 @@
+// 264. Ugly Number II
+
+private data class PrimePointer(val `val`: Int, var pos: Int)
+
+class LeetCode264UglyNumberII {
+    fun nthUglyNumber(n: Int): Int {
+        if (n == 1) return 1
+
+        val dp = IntArray(n)
+        dp[0] = 1
+
+        val pps = arrayOf(
+            PrimePointer(2, 0),
+            PrimePointer(3, 0),
+            PrimePointer(5, 0))
+
+        for (i in 1 until n) {
+            dp[i] = pps.map { p -> p.`val` * dp[p.pos] }.min()!!
+
+            for (p in pps)
+                if (dp[i] == p.`val` * dp[p.pos])
+                    ++p.pos
+        }
+
+        return dp[n-1]
+    }
+}
