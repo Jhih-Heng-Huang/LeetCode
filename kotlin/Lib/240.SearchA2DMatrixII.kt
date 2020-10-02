8ee107617f30af3c53a798d49ebb6a4015d4cdf5
Jhih-Heng-Huang <kklive128@gmail.com>
2020-10-02 09:46:53 +0800

    Solve LeetCode 240. Search a 2D Matrix II in Kotlin again

--- kotlin/Lib/240.SearchA2DMatrixII.kt
+++ kotlin/Lib/240.SearchA2DMatrixII.kt
@@ -2,16 +2,16 @@
 
 class LeetCode240SearchA2DMatrixII {
     fun searchMatrix(matrix: Array<IntArray>, target: Int): Boolean {
-        if (matrix.size == 0)
+        if (matrix.isEmpty())
             return false
-
+        
         var row = 0
         var col = matrix[0].size - 1
-
-        while (row < matrix.size && col >= 0)
+        while (row < matrix.size && col >= 0) {
             if (matrix[row][col] == target) return true
             else if (matrix[row][col] < target) ++row
             else --col
+        }
 
         return false
     }
