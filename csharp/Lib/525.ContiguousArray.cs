e8688e4f5a124241ff0162882eb74a7b8a5d98b4
Jhih-Heng-Huang <kklive128@gmail.com>
2020-11-01 07:46:43 +0800

    Solve LeetCode 525. Contiguous Array in C# again

--- csharp/Lib/525.ContiguousArray.cs
+++ csharp/Lib/525.ContiguousArray.cs
@@ -5,25 +5,38 @@
 using System;
 using System.Collections.Generic;
 
-public class ContiguousArray {
-    public int FindMaxLength(int[] nums) {
-        if (nums == null || nums.Length == 0)
-			return 0;
 
-		var dp = new Dictionary<int, int>();
-		dp[0] = -1;
+public class ContiguousArray {
+	private class DiffLen2IndexDic {
+		private Dictionary<int, int> _dic = new Dictionary<int, int>();
+
+		public void Set(int diffLen, int index) {
+			_dic[diffLen] = index;
+		}
+		public bool TryGetIndex(int diffLen, out int index) {
+			index = -10;
+			if (!_dic.ContainsKey(diffLen)) return false;
+
+			index = _dic[diffLen];
+			return true;
+		}
+	}
+
+    public int FindMaxLength(int[] nums) {
+        if (nums == null || nums.Length == 0) return 0;
 
 		var maxLen = 0;
-		var sum = 0;
+		var dic = new DiffLen2IndexDic();
+		dic.Set(0, -1);
+
+		var diff = 0;
 		for (int i = 0; i < nums.Length; ++i) {
-			sum += nums[i] == 1? 1 : -1;
-
-			if (dp.ContainsKey(sum))
-				maxLen = Math.Max(maxLen, i - dp[sum]);
+			diff += (nums[i] == 0)? 1 : -1;
+			if (dic.TryGetIndex(diff, out var preIndex))
+				maxLen = Math.Max(maxLen, i - preIndex);
 			else
-				dp[sum] = i;
+				dic.Set(diff, i);
 		}
-
 		return maxLen;
     }
 }
