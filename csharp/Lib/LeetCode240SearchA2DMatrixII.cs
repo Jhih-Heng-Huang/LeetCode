cf99a6c60076e16efd40dc59e061a09ae75e7909
Jhih-Heng-Huang <kklive128@gmail.com>
2020-11-28 06:37:21 +0800

    Solve LeetCode: 240. Search a 2D Matrix II in C#

---
+++ csharp/Lib/LeetCode240SearchA2DMatrixII.cs
@@ -0,0 +1,24 @@
+﻿// LeetCode: 240. Search a 2D Matrix II
+
+using System;
+
+public class LeetCode240SearchA2DMatrixII
+{
+    public bool SearchMatrix(int[,] matrix, int target)
+    {
+        if (matrix == null || matrix.Length == 0) return false;
+
+        var rowLength = matrix.GetLength(0);
+        var colLength = matrix.GetLength(1);
+        var row = 0;
+        var col = colLength - 1;
+
+        while (row < rowLength && col >= 0)
+        {
+            if (matrix[row, col] == target) return true;
+            else if (matrix[row, col] < target) ++row;
+            else --col;
+        }
+        return false;
+    }
+}
