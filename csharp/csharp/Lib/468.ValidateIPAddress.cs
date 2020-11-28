30ccf389d552404540ddcb5a69246159fc7fcab2
Jhih-Heng-Huang <kklive128@gmail.com>
2020-11-28 22:28:43 +0800

    Solve LeetCode 468 Validate IP Address

--- csharp/csharp/Lib/468.ValidateIPAddress.cs
+++ csharp/csharp/Lib/468.ValidateIPAddress.cs
@@ -7,68 +7,69 @@ using System.Linq;
 
 public class ValidateIPAddress {
     public string ValidIPAddress(string IP) {
-        if (_IsIPv4(IP))
-			return "IPv4";
-		if (_IsIPv6(IP))
-			return "IPv6";
-		return "Neither";
+        if (_IsIPv4(IP)) return "IPv4";
+        if (_IsIPv6(IP)) return "IPv6";
+        return "Neither";
     }
 
-	private bool _IsIPv4(string ip) {
-		if (ip == null || ip.Length == 0)
-			return false;
-		
-		var strs = ip.Split('.');
-		if (strs.Length != 4)
-			return false;
+    private bool _IsIPv4(string ip)
+    {
+        if (string.IsNullOrEmpty(ip)) return false;
 
-		foreach (var str in strs) {
-			if (str.Length < 1 || str.Length > 3)
-				return false;
+        var ipValues = ip.Split('.');
 
-			if (!_IsDigitalVal(str))
-				return false;
-			
-			if (str[0] == '0' && str.Length > 1)
-				return false;
+        if (ipValues.Length != 4) return false;
+        foreach (var ipValue in ipValues)
+            if (!_IsValidIPv4Value(ipValue))
+                return false;
 
-			var val = int.Parse(str);
-			if (val < 0 || val > 255)
-				return false;
-		}
-		return true;
-	}
+        return true;
+    }
 
-	private bool _IsDigitalVal(string val) {
-		foreach (var c in val.ToCharArray()) {
-			if (!Char.IsDigit(c))
-				return false;
-		}
-		return true;
-	}
+    private bool _IsValidIPv4Value(string ipValue)
+    {
+        if (string.IsNullOrEmpty(ipValue)) return false;
 
-	private bool _IsIPv6(string ip) {
-		if (ip == null || ip.Length == 0)
-			return false;
-		
-		var strs = ip.Split(':');
-		if (strs.Length != 8)
-			return false;
+        if (ipValue.Length > 3) return false;
+        if (ipValue.Any(c => !Char.IsDigit(c))) return false;
+        if (ipValue.Length > 1 && ipValue.First() == '0') return false;
+        var value = int.Parse(ipValue);
+        if (255 < value || value < 0) return false;
 
-		foreach (var str in strs) {
-			if (str.Length < 1 || str.Length > 4)
-				return false;
+        return true;
+    }
 
-			foreach (var c in str.ToCharArray())
-				if (!_IsIPv6Val(c))
-					return false;
-		}
-		return true;
-	}
+    private bool _IsIPv6(string ip)
+    {
+        if (string.IsNullOrEmpty(ip)) return false;
 
-	private bool _IsIPv6Val(char c) {
-		return Char.IsDigit(c) ||
-			(c >= 'a' && c <= 'f') ||
-			(c >= 'A' && c <= 'F');
-	}
+        var ipValues = ip.Split(':');
+
+        if (ipValues.Length != 8) return false;
+
+        foreach (var ipValue in ipValues)
+            if (!_IsIPv6Value(ipValue))
+                return false;
+
+        return true;
+    }
+
+    private bool _IsIPv6Value(string ipValue)
+    {
+        if (string.IsNullOrEmpty(ipValue)) return false;
+
+        if (ipValue.Length > 4 || ipValue.Length < 1)
+            return false;
+
+        for (int i = 0; i < ipValue.Length; ++i)
+        {
+            var c = ipValue[i];
+            if (!Char.IsDigit(c) &&
+                (c < 'a' || c > 'f') &&
+                (c < 'A' || c > 'F'))
+                return false;
+        }
+
+        return true;
+    }
 }
---
+++ csharp/csharp/Tests/LeetCode_486_ValidateIPAddress.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace csharp.Tests
+{
+    class LeetCode_486_ValidateIPAddress
+    {
+        #region Inner classes
+        private class TestCase
+        {
+            public string IpAddress;
+            public string ExpectResult;
+        }
+        #endregion
+
+        [Test]
+        public void TestValidIpV4Address()
+        {
+            var obj = new ValidateIPAddress();
+
+            var testCases = new TestCase[]
+            {
+                new TestCase
+                {
+                    IpAddress = "172.16.254.1",
+                    ExpectResult = "IPv4",
+                },
+                new TestCase
+                {
+                    IpAddress = "192.168.1.0",
+                    ExpectResult = "IPv4",
+                },
+            };
+
+            _CheckResults(obj, testCases);
+        }
+
+        [Test]
+        public void TestInvalidIpV4Address()
+        {
+            var obj = new ValidateIPAddress();
+
+            var testCases = new TestCase[]
+            {
+                new TestCase
+                {
+                    IpAddress = "192.168.1.01",
+                    ExpectResult = "Neither",
+                },
+                new TestCase
+                {
+                    IpAddress = "192.168.1.00",
+                    ExpectResult = "Neither",
+                },
+                new TestCase
+                {
+                    IpAddress = "192.168@1.0",
+                    ExpectResult = "Neither",
+                },
+                new TestCase
+                {
+                    IpAddress = "1e1.4.5.6",
+                    ExpectResult = "Neither",
+                },
+                new TestCase
+                {
+                    IpAddress = "300.4.5.6",
+                    ExpectResult = "Neither",
+                },
+                new TestCase
+                {
+                    IpAddress = "192.168.1",
+                    ExpectResult = "Neither",
+                },
+            };
+
+            _CheckResults(obj, testCases);
+        }
+
+        [Test]
+        public void TestValidIpV6Address()
+        {
+            var obj = new ValidateIPAddress();
+
+            var testCases = new TestCase[]
+            {
+                new TestCase
+                {
+                    IpAddress = "2001:0db8:85a3:0000:0000:8a2e:0370:7334",
+                    ExpectResult = "IPv6",
+                },
+                new TestCase
+                {
+                    IpAddress = "2001:0db8:85a3:0:0:8a2e:0370:7334",
+                    ExpectResult = "IPv6",
+                },
+            };
+
+            _CheckResults(obj, testCases);
+        }
+
+        [Test]
+        public void TestInvalidIpV6Address()
+        {
+            var obj = new ValidateIPAddress();
+
+            var testCases = new TestCase[]
+            {
+                new TestCase
+                {
+                    IpAddress = "2001:0db8:85a3::8a2e:0370:7334",
+                    ExpectResult = "Neither",
+                },
+                new TestCase
+                {
+                    IpAddress = "02001:0db8:85a3:0000:0000:8a2e:0370:7334",
+                    ExpectResult = "Neither",
+                },
+                new TestCase
+                {
+                    IpAddress = "20EE:FGb8:85a3:0:0:8A2E:0370:7334",
+                    ExpectResult = "Neither",
+                },
+            };
+
+            _CheckResults(obj, testCases);
+        }
+
+        private void _CheckResults(ValidateIPAddress obj, TestCase[] testCases)
+        {
+            foreach (var testCase in testCases)
+            {
+                Assert.AreEqual(testCase.ExpectResult, obj.ValidIPAddress(testCase.IpAddress));
+            }
+        }
+    }
+}
