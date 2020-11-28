f31145a0b9db3eb6477cf08020b2438a81f40bfd
Jhih-Heng-Huang <kklive128@gmail.com>
2020-11-28 14:58:36 +0800

    Update C# LeetCode project

---
+++ csharp/csharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace csharp
+{
+    class Program
+    {
+        public static void main(string[] args)
+        {
+            Console.WriteLine("Hello Albert!");
+        }
+    }
+}
