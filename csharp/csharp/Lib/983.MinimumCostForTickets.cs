95baccc9437add42f00a21ce84f034e52f1db614
Jhih-Heng-Huang <kklive128@gmail.com>
2020-12-03 20:39:13 +0800

    Solve LeetCode: 983. Minimum Cost For Tickets in C# again

--- csharp/csharp/Lib/983.MinimumCostForTickets.cs
+++ csharp/csharp/Lib/983.MinimumCostForTickets.cs
@@ -3,55 +3,59 @@ using System.Linq;
 
 public class Solution {
 	private class Ticket {
-		public int Cost;
-		public int Period;
+		public int Price = 0;
+		public int Period = 0;
 	}
 
-    public int MincostTickets(int[] days, int[] costs) {
+	public int MincostTickets(int[] days, int[] costs) {
 		if (days == null || days.Length == 0 ||
 			costs == null || costs.Length == 0)
 			return 0;
-		
 		var tickets = _GenTickets(costs);
-		return _GetMinCost(days, tickets);
-    }
+		return _MinCost(days, tickets);
+	}
 
 	private Ticket[] _GenTickets(int[] costs) {
-		return new Ticket[] {
-			new Ticket {
-				Cost = costs[0],
-				Period = 1,
-			},
-			new Ticket {
-				Cost = costs[1],
-				Period = 7,
-			},
-			new Ticket {
-				Cost = costs[2],
-				Period = 30,
-			},
+		return new Ticket[]
+		{
+			new Ticket {Price = costs[0], Period = 1},
+			new Ticket {Price = costs[1], Period = 7},
+			new Ticket {Price = costs[2], Period = 30},
 		};
 	}
 
-	private int _GetMinCost(int[] days, Ticket[] tickets) {
+	private int _MinCost(int[] days, Ticket[] tickets) {
+
 		var dp = new int[days.Length];
 
 		for (int i = 0; i < dp.Length; ++i) {
-			var minCost = int.MaxValue;
+			var date = days[i];
+			dp[i] = _MinCost(date, days, tickets, dp);
+		}
+		return dp.Last();
+	}
 
-			foreach (var ticket in tickets) {
-				var cost = ticket.Cost;
-				var lastDay = days[i] - ticket.Period;
-				var index = days.Where(d => d <= lastDay).Count()-1;
-
-				if (index >= 0)
-					cost += dp[index];
-
-				minCost = Math.Min(minCost, cost);
-			}
-			dp[i] = minCost;
+	private int _MinCost(int date, int[] days, Ticket[] tickets, int[] dp) {
+		var minPrice = int.MaxValue;
+		foreach (var ticket in tickets) {
+			var price = ticket.Price;
+			var lastDateIndex = _GetLastDateIndex(date - ticket.Period, days);
+			
+			if (lastDateIndex != -1)
+				price += dp[lastDateIndex];
+			
+			minPrice = Math.Min(minPrice, price);
 		}
 
-		return dp.Last();
+		return minPrice;
+	}
+
+	private int _GetLastDateIndex(int date, int[] days) {
+		var lastIndex = -1;
+		for (int i = 0; i < days.Length; ++i) {
+			if (days[i] > date) return lastIndex;
+			else lastIndex = i;
+		}
+		return lastIndex;
 	}
 }
