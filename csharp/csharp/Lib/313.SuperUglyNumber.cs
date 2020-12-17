ac049ff2209ce79b6368d141d6fc8b7578e080d8
Jhih-Heng-Huang <kklive128@gmail.com>
2020-12-18 07:00:06 +0800

    Solve 313. Super Ugly Number in C# again

--- csharp/csharp/Lib/313.SuperUglyNumber.cs
+++ csharp/csharp/Lib/313.SuperUglyNumber.cs
@@ -7,33 +7,32 @@ using System.Linq;
 public class SuperUglyNumber {
 	private class PrimePointer {
 		public int Value;
-		public int Pointer;
+		public int Position;
 	}
 
     public int NthSuperUglyNumber(int n, int[] primes) {
-		if (n == 1)
-			return 1;
+		if (n == 1) return 1;
 		
-		var pps = _GenPrimePointers(primes);
-		var dp = new int[n];
-		dp[0] = 1;
-
-		for (int i = 1; i < dp.Length; ++i) {
-			dp[i] = pps.Min(p => p.Value * dp[p.Pointer]);
-
-			foreach (var p in pps)
-				if (dp[i] == p.Value * dp[p.Pointer])
-					++p.Pointer;
-		}
-
-		return dp[n-1];
+		var primePointers = primes.Select(prime =>
+			new PrimePointer{Value = prime, Position = 0})
+			.ToArray();
+		
+		return _FindNthSuperUglyNumber(n, primePointers);
     }
 
-	private PrimePointer[] _GenPrimePointers(int[] primes) {
-		return primes.Select(p =>
-			new PrimePointer {
-				Value = p,
-				Pointer = 0,
-			}).ToArray();
+	private int _FindNthSuperUglyNumber(int n, PrimePointer[] primePointers) {
+		var table = new int[n];
+		table[0] = 1;
+
+		for (int i = 1; i < n; i++) {
+			table[i] = primePointers.Min(primePointer => primePointer.Value * table[primePointer.Position]);
+
+			foreach (var primePointer in primePointers) {
+				if (table[i] == primePointer.Value * table[primePointer.Position])
+					++primePointer.Position;
+			}
+		}
+
+		return table[n-1];
 	}
 }
