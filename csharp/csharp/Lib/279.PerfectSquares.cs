b5c8d723c6c0c6300528f923c2614673c28dc884
Jhih-Heng-Huang <kklive128@gmail.com>
2020-12-18 06:59:38 +0800

    Solve LeetCode: 279. Perfect Squares in C# again

--- csharp/csharp/Lib/279.PerfectSquares.cs
+++ csharp/csharp/Lib/279.PerfectSquares.cs
@@ -5,29 +5,33 @@ using System;
 namespace csharp.Lib {
 	public class LeetCode279PerfectSquares {
 		public int NumSquares(int n) {
-			if (n < 1) return 0;
+			if (n <= 1) return n;
 
-			var table = new int[n+1];
-			table[0] = 0;
+			var table = _GenTable(n);
 
 			for (int i = 1; i < table.Length; ++i) {
-				table[i] = _FindLeastNumSquares(i, table);
+				table[i] = _GetMinNumSquares(i, table);
 			}
 
 			return table[n];
 		}
 
-		private int _FindLeastNumSquares(in int n, in int[] table) {
-			var length = (int)Math.Floor(Math.Sqrt(n));
-
-			var leastNumSquares = int.MaxValue;
-			for (int i = 1; i <= length; ++i) {
-				var numSquares = 1 + table[n - i*i];
-				if (leastNumSquares > numSquares)
-					leastNumSquares = numSquares;
-			}
-
-			return leastNumSquares;
+		private int[] _GenTable(int n) {
+			var table = new int[1+n];
+			table[0] = 0;
+			return table;
 		}
+
+		private int _GetMinNumSquares(int n, int[] table)
+        {
+			var upperBound = Math.Sqrt(n);
+			var minNum = int.MaxValue;
+			for (int i = 1; i <= upperBound; ++i)
+            {
+				minNum = Math.Min(minNum, 1 + table[n - i * i]);
+            }
+
+			return minNum;
+        }
 	}
 }
