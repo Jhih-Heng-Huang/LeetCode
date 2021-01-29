d0ade460bd5508164f85fb937128dcf44a902cc4
Jhih-Heng-Huang <kklive128@gmail.com>
2021-01-30 06:44:09 +0800

    Solve LeetCode: 240. Search a 2D Matrix II in C# again

--- csharp/csharp/Lib/240.SearchA2DMatrixII.cs
+++ csharp/csharp/Lib/240.SearchA2DMatrixII.cs
@@ -4,21 +4,22 @@ using System;
 
 public class LeetCode240SearchA2DMatrixII
 {
-    public bool SearchMatrix(int[,] matrix, int target)
-    {
-        if (matrix == null || matrix.Length == 0) return false;
+	public bool SearchMatrix(int[][] matrix, int target) {
+		if (matrix == null ||
+			matrix.Length == 0 ||
+			matrix[0].Length == 0)
+			return false;
 
-        var rowLength = matrix.GetLength(0);
-        var colLength = matrix.GetLength(1);
-        var row = 0;
-        var col = colLength - 1;
+		var rowIndex = 0;
+		var colIndex = matrix[0].Length - 1;
 
-        while (row < rowLength && col >= 0)
-        {
-            if (matrix[row, col] == target) return true;
-            else if (matrix[row, col] < target) ++row;
-            else --col;
-        }
-        return false;
-    }
+		while (rowIndex < matrix.Length && colIndex >= 0) {
+			var value = matrix[rowIndex][colIndex];
+			if (value == target) return true;
+			else if (value > target) --colIndex;
+			else ++rowIndex;
+		}
+
+		return false;
+	}
 }
