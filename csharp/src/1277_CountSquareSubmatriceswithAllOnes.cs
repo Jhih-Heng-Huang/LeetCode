8b1be90378f0229eb45199c386b4eed8217ce961
Jhih-Heng-Huang <kklive128@gmail.com>
2021-03-06 22:19:18 +0800

    Solve LeetCode: 1277. Count Square Submatrices with All Ones in C#

--- csharp/src/1277_CountSquareSubmatriceswithAllOnes.cs
+++ csharp/src/1277_CountSquareSubmatriceswithAllOnes.cs
@@ -1,5 +1,5 @@
 /*
-1277. Count Square Submatrices with All Ones
+LeetCode: 1277. Count Square Submatrices with All Ones
 */
 
 using System;
@@ -7,39 +7,47 @@ using System.Linq;
 
 public class CountSquareSubmatriceswithAllOnes {
     public int CountSquares(int[][] matrix) {
-		if (matrix == null || matrix.Length == 0)
+		if (matrix == null ||
+			matrix.Length == 0 ||
+			matrix[0].Length == 0)
 			return 0;
 		
-
-		return _CountSquare(matrix);
+		return _CountSquares(matrix);
     }
 
-	private int _CountSquare(int[][] matrix) {
-		var dp = _GenTable(matrix.Length, matrix[0].Length);
+	private int _CountSquares(int[][] matrix)
+	{
+		var table = _GenTable(matrix.Length, matrix[0].Length);
 
-		var count = 0;
-		for (int i = 0; i < dp.Length; ++i)
-			for (int j = 0; j < dp[i].Length; ++j) {
-				if (i == 0 || j == 0) dp[i][j] = matrix[i][j];
-				else if (matrix[i][j] == 0) dp[i][j] = 0;
-				else dp[i][j] = 1 + new int[] {
-					dp[i-1][j],
-					dp[i-1][j-1],
-					dp[i][j-1],
-				}.Min();
-				count += dp[i][j];
+		for (int row = 0; row < matrix.Length; ++row)
+			for (int col = 0; col < matrix[row].Length; ++col)
+			{
+				if (row == 0 || col == 0)
+					table[row][col] = matrix[row][col];
+				else if (matrix[row][col] == 0)
+					table[row][col] = 0;
+				else
+					table[row][col] = 1 + new int[] {
+						table[row-1][col],
+						table[row][col-1],
+						table[row-1][col-1]}.Min();
 			}
-
+		
+		var count = 0;
+		foreach (var row in table)
+			foreach (var ele in row)
+				count += ele;
+		
 		return count;
 	}
 
 	private int[][] _GenTable(int numRows, int numCols) {
 		var table = new int[numRows][];
 
-		for (int i = 0; i < numRows; ++i) {
-			table[i] = new int[numCols];
-			for (int j = 0; j < numCols; ++j)
-				table[i][j] = 0;
+		for (int row = 0; row < numRows; ++row) {
+			table[row] = new int[numCols];
+			for (int col = 0; col < numCols; ++col)
+				table[row][col] = 0;
 		}
 
 		return table;
