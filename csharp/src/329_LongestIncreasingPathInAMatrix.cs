31377dd964fce53ae10cc44b431c0f67cbd06753
Jhih-Heng-Huang <kklive128@gmail.com>
2021-03-07 10:03:47 +0800

    Solve LeetCode: 329. Longest Increasing Path in a Matrix in C#

--- csharp/src/329_LongestIncreasingPathInAMatrix.cs
+++ csharp/src/329_LongestIncreasingPathInAMatrix.cs
@@ -1,5 +1,5 @@
 /*
-329. Longest Increasing Path in a Matrix
+LeetCode: 329. Longest Increasing Path in a Matrix
 */
 
 using System;
@@ -7,74 +7,79 @@ using System.Collections.Generic;
 using System.Linq;
 
 public class LongestIncreasingPathInAMatrix {
-    public int LongestIncreasingPath(int[][] matrix) 
-    {
-		if (matrix == null || matrix.Length == 0)
+	private class Position {
+		public int row;
+		public int col;
+	}
+
+	public int LongestIncreasingPath(int[][] matrix) 
+	{
+		if (matrix == null ||
+			matrix.Length == 0 ||
+			matrix[0].Length == 0)
 			return 0;
 		
-		return _FindLongestPath(matrix);
+		var table = new int[matrix.Length][];
+		for (int row = 0; row < table.Length; ++row)
+		{
+			table[row] = new int[matrix[row].Length];
+			for (int col = 0; col < table[row].Length; ++col)
+				table[row][col] = 0;
+		}
+
+		int result = 0;
+		for (int row = 0; row < table.Length; ++row)
+			for (int col = 0; col < table[row].Length; ++col)
+				result = Math.Max(result,
+				_FindLongestIncreasingPath(row, col, matrix, table));
+		
+		return result;
     }
 
-	private int _FindLongestPath(int[][] matrix) {
-		var dp = _GenTable(matrix.Length, matrix[0].Length);
-
-		var max = 0;
-		for (int i = 0; i < dp.Length; ++i)
-			for (int j = 0; j < dp[i].Length; ++j)
-				if (dp[i][j] == -1)
-					max = Math.Max(max, _FindLongestPath(i, j, matrix, dp));
-		return max;
-	}
-
-	private int _FindLongestPath(int i, int j,
-		int[][] matrix, int[][] dp)
+	private int _FindLongestIncreasingPath(
+		int row, int col,
+		int[][] matrix, int[][] table)
 	{
-		if (dp[i][j] != -1)
-			return dp[i][j];
+		if (table[row][col] != 0) return table[row][col];
 
-		var len = 1;
-		var paths = _FindPath(i, j, matrix);
-
+		var result = 1;
+		var paths = _FindPaths(row, col, matrix);
 		foreach (var path in paths) {
-			var x = path[0];
-			var y = path[1];
-
-			var subLen = _FindLongestPath(x, y, matrix, dp);
-			len = Math.Max(len, 1+subLen);
+			if (matrix[row][col] < matrix[path.row][path.col])
+				result = Math.Max(result,
+				1 + _FindLongestIncreasingPath(path.row, path.col, matrix, table));
 		}
-
-		dp[i][j] = len;
-		return len;
+		table[row][col] = result;
+		return result;
 	}
 
-	private int[][] _GenTable(int numRows, int numCols) {
-		var table = new int[numRows][];
+	private Position[] _FindPaths(int row, int col, int[][] matrix) {
+		var list = new List<Position>();
 
-		for (int i = 0; i < numRows; ++i) {
-			table[i] = new int[numCols];
-			for (int j = 0; j < numCols; ++j)
-				table[i][j] = -1;
-		}
-
-		return table;
-	}
-
-	private int[][] _FindPath(int i, int j, int[][] matrix) {
-		var paths = new List<int[]>();
-		var val = matrix[i][j];
-
-		if (i > 0 && val < matrix[i-1][j])
-			paths.Add(new int[]{i-1, j});
-
-		if (j > 0 && val < matrix[i][j-1])
-			paths.Add(new int[]{i, j-1});
-
-		if (i < matrix.Length-1 && val < matrix[i+1][j])
-			paths.Add(new int[]{i+1, j});
-
-		if (j < matrix[i].Length-1 && val < matrix[i][j+1])
-			paths.Add(new int[]{i, j+1});
-
-		return paths.ToArray();
+		if (row - 1 >= 0)
+			list.Add(new Position
+			{
+				row = row-1,
+				col = col,
+			});
+		if (col - 1 >= 0)
+			list.Add(new Position
+			{
+				row = row,
+				col = col-1,
+			});
+		if (row + 1 < matrix.Length)
+			list.Add(new Position
+			{
+				row = row+1,
+				col = col,
+			});
+		if (col + 1 < matrix[row].Length)
+			list.Add(new Position
+			{
+				row = row,
+				col = col+1,
+			});
+		return list.ToArray();
 	}
 }
