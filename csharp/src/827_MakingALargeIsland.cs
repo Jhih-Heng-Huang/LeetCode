bd1b2bed231f4656baa72bf15720261b7183b2b5
Jhih-Heng-Huang <kklive128@gmail.com>
2021-03-14 16:47:37 +0800

    Solve LeetCode: 827. Making A Large Island in C#

--- csharp/src/827_MakingALargeIsland.cs
+++ csharp/src/827_MakingALargeIsland.cs
@@ -1,99 +1,96 @@
-// 827. Making A Large Island
+// LeetCode: 827. Making A Large Island
 
 using System;
 using System.Collections.Generic;
 using System.Linq;
 
 public class LeetCode827MakingALargeIsland {
-	private Dictionary<int, int> _colorSizeMap = new Dictionary<int, int>();
-
 	public int LargestIsland(int[][] grid) {
-		if (grid == null || grid.Length == 0) return 0;
-
-		// color the island
-		_PaintColorIsland(grid);
-		var maxSize = _GetLargestColorIsland();
-		maxSize = Math.Max(maxSize, _GetLargestConnectedIsland(grid));
+		if (grid == null ||
+			grid.Length == 0 ||
+			grid[0].Length == 0)
+			return 0;
 		
+		var map = (int[][]) grid.Clone();
+
+		var color = 2;
+		var color2Size = new Dictionary<int, int>();
+		for (int row = 0; row < map.Length; ++row)
+			for (int col = 0; col < map[row].Length; ++col)
+			{
+				if (map[row][col] == 1)
+				{
+					color2Size.Add(color, _PaintIsland(row, col, color, map));
+					++color;
+				}
+			}
+		
+		var maxSize = 0;
+		foreach (var size in color2Size.Values)
+			maxSize = Math.Max(maxSize, size);
+
+		for (int row = 0; row < map.Length; ++row)
+			for (int col = 0; col < map[row].Length; ++col)
+			{
+				if (map[row][col] == 0)
+					maxSize = Math.Max(
+						maxSize,
+						_CombineSize(row, col, map, color2Size));
+			}
+
 		return maxSize;
 	}
 
-	private void _PaintColorIsland(int[][] map) {
-		_colorSizeMap.Clear();
-
-		int color = 2;
-		for (int x = 0; x < map.Length; ++x) {
-			for (int y = 0; y < map[x].Length; ++y) {
-				if (map[x][y] != 1) continue;
-				var size = _GetColorIslandSize(x, y, color, map);
-				_colorSizeMap[color] = size;
-				++color;
-			}
-		}
+	private int[][] _GetPaths(int row, int col, int[][] map)
+	{
+		var paths = new List<int[]>();
+		if (row - 1 >= 0)
+			paths.Add(new int[]{row-1, col});
+		if (col - 1 >= 0)
+			paths.Add(new int[]{row, col-1});
+		if (row + 1 < map.Length)
+			paths.Add(new int[]{row+1, col});
+		if (col + 1 < map[row].Length)
+			paths.Add(new int[]{row, col+1});
+		return paths.ToArray();
 	}
 
-	private int _GetColorIslandSize(int x, int y, int color, int[][] map) {
-		if (map[x][y] != 1) return 0;
+	private int _PaintIsland(
+		int row, int col, int color,
+		int[][] map)
+	{
+		if (map[row][col] != 1) return 0;
 
-		map[x][y] = color;
-		var paths = _GetPaths(x, y, map);
+		map[row][col] = color;
 		var size = 1;
-
+		var paths = _GetPaths(row, col, map);
 		foreach (var path in paths)
 		{
 			var i = path[0];
 			var j = path[1];
-			size += _GetColorIslandSize(i, j, color, map);
+			size += _PaintIsland(i, j, color, map);
 		}
-
 		return size;
 	}
 
-	private int[][] _GetPaths(int x, int y, int[][] map) {
-		var paths = new List<int[]>();
+	private int _CombineSize(int row, int col, int[][] map, Dictionary<int, int> color2Size)
+	{
+		if (map[row][col] != 0) return 0;
 
-		if (x > 0) paths.Add(new int[]{x-1,y});
-		if (y > 0) paths.Add(new int[]{x,y-1});
-		if (x < map.Length - 1) paths.Add(new int[]{x+1,y});
-		if (y < map[x].Length - 1) paths.Add(new int[]{x,y+1});
-
-		return paths.ToArray();
-	}
-
-	private int _GetLargestColorIsland() {
-		if (_colorSizeMap.Keys.Count() > 0)
-			return _colorSizeMap.Values.Max();
-		return 0;
-	}
-
-	private int _GetLargestConnectedIsland(int[][] map) {
-		var maxSize = 0;
-		for (int x = 0; x < map.Length; ++x) {
-			for (int y = 0; y < map[x].Length; ++y) {
-				var size = _GetLargestConnectedIsland(x, y, map);
-				maxSize = Math.Max(maxSize, size);
-			}
-		}
-
-		return maxSize;
-	}
-
-	private int _GetLargestConnectedIsland(int x, int y, int[][] map) {
-		if (map[x][y] != 0) return 0;
-
-		var paths = _GetPaths(x, y, map);
-		var colorHashSet = new HashSet<int>();
 		var size = 1;
+		var paths = _GetPaths(row, col, map);
+		var colorCache = new HashSet<int>();
 		foreach (var path in paths)
 		{
 			var i = path[0];
 			var j = path[1];
 			var color = map[i][j];
-			if (color == 0 || colorHashSet.Contains(color)) continue;
-			colorHashSet.Add(color);
-			size += _colorSizeMap[color];
+			if (color != 0 && !colorCache.Contains(color))
+			{
+				colorCache.Add(color);
+				size += color2Size[color];
+			}
 		}
-
 		return size;
 	}
 }
