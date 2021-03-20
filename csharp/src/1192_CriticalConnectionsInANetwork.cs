40ff74b8c08fd92b627e5e010ca9cc06b2104cd3
Jhih-Heng-Huang <kklive128@gmail.com>
2021-03-20 19:26:41 +0800

    Solve LeetCode: 1192. Critical Connections in a Network in C#

--- csharp/src/1192_CriticalConnectionsInANetwork.cs
+++ csharp/src/1192_CriticalConnectionsInANetwork.cs
@@ -7,79 +7,70 @@ using System.Collections.Generic;
 using System.Linq;
 
 public class CriticalConnectionsInANetwork {
-    private class Node
-    {
-        public int Rank = -2;
-        public List<int> Nexts = new List<int>();
-    }
+	private class Node {
+		public int Rank = -2;
+		public List<int> Nexts = new List<int>();
+	}
 
-    public IList<IList<int>> CriticalConnections(
-        int n, IList<IList<int>> connections)
-    {
-        var list = new List<IList<int>>();
-        if (n == 0 || connections == null || connections.Count == 0)
-            return list;
+	public IList<IList<int>> CriticalConnections(
+		int n, IList<IList<int>> connections)
+	{
+		if (n == 0 || connections == null ||
+			connections.Count == 0)
+			return new List<IList<int>>();
 
-        var nodes = _GenGraph(n, connections);
+		var nodes = _GenGraph(n, connections);
+		var criticalEdges = new List<IList<int>>();
 
-        for (int i = 0; i < nodes.Length; ++i)
-        {
-            if (nodes[i].Rank == -2)
-                _Traverse(i, 0, nodes, list);
-        }
+		for (int i = 0; i < nodes.Length; ++i)
+			if (nodes[i].Rank == -2)
+				_GetMinRank(i, 0, nodes, criticalEdges);
 
-        return list;
-    }
+		return criticalEdges;
+	}
 
-    private Node[] _GenGraph(int num, IList<IList<int>> edges)
-    {
-        var nodes = new Node[num];
+	private Node[] _GenGraph(int n, IList<IList<int>> edges)
+	{
+		var nodes = new Node[n];
 
-        for (int i = 0; i < nodes.Length; ++i)
-            nodes[i] = new Node();
+		for (int i = 0; i < nodes.Length; ++i)
+			nodes[i] = new Node();
 
-        foreach (var edge in edges)
-        {
-            var i = edge[0];
-            var j = edge[1];
-            nodes[i].Nexts.Add(j);
-            nodes[j].Nexts.Add(i);
-        }
+		foreach (var edge in edges) {
+			var i = edge[0];
+			var j = edge[1];
+			nodes[i].Nexts.Add(j);
+			nodes[j].Nexts.Add(i);
+		}
 
-        return nodes;
-    }
+		return nodes;
+	}
 
-    private bool _IsVisited(Node node)
-    {
-        return node.Rank >= 0;
-    }
+	private int _GetMinRank(int i, int rank, Node[] nodes, IList<IList<int>> criticalEdges)
+	{
+		if (nodes[i].Rank != -2) return nodes[i].Rank;
 
+		nodes[i].Rank = rank;
+		var minRank = rank;
+		var num = nodes.Length;
 
-    private int _Traverse(int i, int rank, Node[] nodes, IList<IList<int>> criticalEdges)
-    {
-        if (nodes[i].Rank != -2) return nodes[i].Rank;
+		foreach (var next in nodes[i].Nexts)
+		{
+			if (nodes[next].Rank == rank-1 ||
+				nodes[next].Rank == num)
+				continue;
+			
+			var nextRank = _GetMinRank(next, rank + 1,
+				nodes, criticalEdges);
+			
+			if (rank >= nextRank)
+				minRank = Math.Min(minRank, nextRank);
+			else {
+				criticalEdges.Add(new int[]{i, next}.ToList());
+			}
+		}
 
-        var num = nodes.Length;
-        nodes[i].Rank = rank;
-        var minRank = rank;
-
-        foreach (var next in nodes[i].Nexts)
-        {
-            var nextRank = nodes[next].Rank;
-            if (nextRank == rank - 1 || nextRank == num)
-                continue;
-
-            nextRank = _Traverse(next, rank + 1, nodes, criticalEdges);
-            if (rank >= nextRank)
-                minRank = Math.Min(minRank, nextRank);
-            else
-            {
-                var edge = new int[] { i, next }.ToList();
-                criticalEdges.Add(edge);
-            }
-        }
-
-        nodes[i].Rank = num;
-        return minRank;
-    }
+		nodes[i].Rank = num;
+		return minRank;
+	}
 }
