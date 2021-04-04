1268b489ea349c3a508f0b9ae37fc4893972612f
Jhih-Heng-Huang <kklive128@gmail.com>
2021-04-04 18:17:44 +0800

    Solve LeetCode: 468. Validate IP Address in C#

--- csharp/src/468_ValidateIPAddress.cs
+++ csharp/src/468_ValidateIPAddress.cs
@@ -2,74 +2,79 @@
 LeetCode: 468. Validate IP Address
 */
 
-using System;
-using System.Linq;
+namespace LeetCode.Problem_468
+{    
+	public class ValidateIPAddress {
+		public string ValidIPAddress(string IP)
+		{
+			if (_IsIPV4(IP)) return "IPv4";
+			if (_IsIPV6(IP)) return "IPv6";
+			return "Neither";
+		}
 
-public class ValidateIPAddress {
-    public string ValidIPAddress(string IP) {
-        if (_IsIPv4(IP)) return "IPv4";
-        if (_IsIPv6(IP)) return "IPv6";
-        return "Neither";
-    }
+		private bool _IsIPV4(string ip)
+		{
+			if (string.IsNullOrEmpty(ip))
+				return false;
+			
+			var values = ip.Split('.');
 
-    private bool _IsIPv4(string ip)
-    {
-        if (string.IsNullOrEmpty(ip)) return false;
+			if (values.Length != 4) return false;
 
-        var ipValues = ip.Split('.');
+			foreach (var value in values)
+				if (!_IsValidIPV4Value(value))
+					return false;
+			return true;
+		}
 
-        if (ipValues.Length != 4) return false;
-        foreach (var ipValue in ipValues)
-            if (!_IsValidIPv4Value(ipValue))
-                return false;
+		private bool _IsValidIPV4Value(string value)
+		{
+			if (string.IsNullOrEmpty(value)) return false;
+			if (value.Length < 1 || value.Length > 3)
+				return false;
+			if (value[0] == '0' && value.Length > 1)
+				return false;
 
-        return true;
-    }
+			int result = 0;
+			if (!int.TryParse(value, out result))
+				return false;
+			if (result < 0 || result > 255)
+				return false;
 
-    private bool _IsValidIPv4Value(string ipValue)
-    {
-        if (string.IsNullOrEmpty(ipValue)) return false;
+			return true;
+		}
 
-        if (ipValue.Length > 3) return false;
-        if (ipValue.Any(c => !Char.IsDigit(c))) return false;
-        if (ipValue.Length > 1 && ipValue.First() == '0') return false;
-        var value = int.Parse(ipValue);
-        if (255 < value || value < 0) return false;
+		private bool _IsIPV6(string ip)
+		{
+			if (string.IsNullOrEmpty(ip))
+				return false;
+			
+			var values = ip.Split(':');
 
-        return true;
-    }
+			if (values.Length != 8) return false;
 
-    private bool _IsIPv6(string ip)
-    {
-        if (string.IsNullOrEmpty(ip)) return false;
+			foreach (var value in values)
+				if (!_IsValidIPV6Value(value))
+					return false;
 
-        var ipValues = ip.Split(':');
+			return true;
+		}
 
-        if (ipValues.Length != 8) return false;
+		private bool _IsValidIPV6Value(string value)
+		{
+			if (string.IsNullOrEmpty(value))
+				return false;
+			
+			if (value.Length < 1 || value.Length > 4)
+				return false;
+			
+			foreach (var c in value)
+				if (!char.IsDigit(c) &&
+					!('a' <= c && c <= 'f') &&
+					!('A' <= c && c <= 'F'))
+					return false;
+			return true;
+		}
+	}
 
-        foreach (var ipValue in ipValues)
-            if (!_IsIPv6Value(ipValue))
-                return false;
-
-        return true;
-    }
-
-    private bool _IsIPv6Value(string ipValue)
-    {
-        if (string.IsNullOrEmpty(ipValue)) return false;
-
-        if (ipValue.Length > 4 || ipValue.Length < 1)
-            return false;
-
-        for (int i = 0; i < ipValue.Length; ++i)
-        {
-            var c = ipValue[i];
-            if (!Char.IsDigit(c) &&
-                (c < 'a' || c > 'f') &&
-                (c < 'A' || c > 'F'))
-                return false;
-        }
-
-        return true;
-    }
 }
