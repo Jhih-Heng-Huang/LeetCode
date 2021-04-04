8ceb6abca633fc4a83fcb049fb7bec8d82d7f678
Jhih-Heng-Huang <kklive128@gmail.com>
2021-04-04 16:11:36 +0800

    Solve LeetCode 72. Edit Distance in C#

--- csharp/src/72_EditDistance.cs
+++ csharp/src/72_EditDistance.cs
@@ -1,53 +1,50 @@
 /*
-72. Edit Distance
+LeetCode 72. Edit Distance
 */
 
 using System;
-using System.Linq;
 
-public class EditDistance {
-    public int MinDistance(string word1, string word2)
-    {
-        if (string.IsNullOrEmpty(word1) && string.IsNullOrEmpty(word2))
-            return 0;
-        if (string.IsNullOrEmpty(word1)) return word2.Length;
-        if (string.IsNullOrEmpty(word2)) return word1.Length;
+namespace LeetCode.Problem_72
+{
+	public class EditDistance {
+		public int MinDistance(string word1, string word2)
+		{
+			if (string.IsNullOrEmpty(word1) &&
+				string.IsNullOrEmpty(word2)) return 0;
+			if (string.IsNullOrEmpty(word1)) return word2.Length;
+			if (string.IsNullOrEmpty(word2)) return word1.Length;
 
+			return _GetMinEditDistance(word1, word2);
+		}
 
-        return _GetMinDistance(word1, word2);
-    }
+		private int _GetMinEditDistance(string word1, string word2)
+		{
+			var table = _GenTable(
+				rowNum: word1.Length + 1,
+				colNum: word2.Length + 1);
 
-    private int _GetMinDistance(string word1, string word2)
-    {
-        var table = _GenTable(word1.Length, word2.Length);
+			for (int row = 0; row < table.Length; ++row)
+				for (int col = 0; col < table[row].Length; ++col)
+					if (row == 0)
+						table[row][col] = col;
+					else if (col == 0)
+						table[row][col] = row;
+					else if (word1[row-1] == word2[col-1])
+						table[row][col] = table[row-1][col-1];
+					else
+						table[row][col] = 1 + Math.Min(table[row-1][col-1],
+							Math.Min(table[row-1][col], table[row][col-1]));
+			return table[word1.Length][word2.Length];
+		}
 
-        for (int i = 0; i < table.Length; ++i)
-            for (int j = 0; j < table[i].Length; ++j)
-            {
-                int dist = 0;
-                if (i == 0) dist = j;
-                else if (j == 0) dist = i;
-                else if (word1[i - 1] == word2[j - 1])
-                    dist = table[i - 1][j - 1];
-                else
-                    dist = 1 + new int[]
-                    {
-                        table[i-1][j],
-                        table[i-1][j-1],
-                        table[i][j-1],
-                    }.Min();
-                table[i][j] = dist;
-            }
+		private int[][] _GenTable(int rowNum, int colNum)
+		{
+			var table = new int[rowNum][];
 
-        return table[word1.Length][word2.Length];
-    }
-
-    private int[][] _GenTable(int word1Length, int word2Length)
-    {
-        var table = new int[word1Length + 1][];
-        for (int i = 0; i < table.Length; ++i)
-            table[i] = new int[word2Length + 1];
-
-        return table;
-    }
+			for (int row = 0; row < table.Length; ++row)
+				table[row] = new int[colNum];
+			
+			return table;
+		}
+	}
 }
