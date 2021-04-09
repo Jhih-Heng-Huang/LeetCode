53f5cd3ab764839f355d7a3a4fbc3267e88b986b
Jhih-Heng-Huang <kklive128@gmail.com>
2021-04-09 20:39:52 +0800

    Solve LeetCode: 983. Minimum Cost For Tickets in C#

--- csharp/src/983_MinimumCostForTickets.cs
+++ csharp/src/983_MinimumCostForTickets.cs
@@ -1,61 +1,71 @@
+/*
+LeetCode: 983. Minimum Cost For Tickets
+*/
+
 using System;
 using System.Linq;
 
-public class Solution {
-	private class Ticket {
-		public int Price = 0;
-		public int Period = 0;
-	}
-
-	public int MincostTickets(int[] days, int[] costs) {
-		if (days == null || days.Length == 0 ||
-			costs == null || costs.Length == 0)
-			return 0;
-		var tickets = _GenTickets(costs);
-		return _MinCost(days, tickets);
-	}
-
-	private Ticket[] _GenTickets(int[] costs) {
-		return new Ticket[]
+namespace LeetCode.Problem_983
+{	
+	public class Solution
+	{
+		private class Ticket
 		{
-			new Ticket {Price = costs[0], Period = 1},
-			new Ticket {Price = costs[1], Period = 7},
-			new Ticket {Price = costs[2], Period = 30},
-		};
-	}
-
-	private int _MinCost(int[] days, Ticket[] tickets) {
-
-		var dp = new int[days.Length];
-
-		for (int i = 0; i < dp.Length; ++i) {
-			var date = days[i];
-			dp[i] = _MinCost(date, days, tickets, dp);
+			public int Cost;
+			public int Period;
 		}
-		return dp.Last();
-	}
 
-	private int _MinCost(int date, int[] days, Ticket[] tickets, int[] dp) {
-		var minPrice = int.MaxValue;
-		foreach (var ticket in tickets) {
-			var price = ticket.Price;
-			var lastDateIndex = _GetLastDateIndex(date - ticket.Period, days);
+		public int MincostTickets(int[] days, int[] costs)
+		{
+			if (days == null || days.Length == 0)
+				return 0;
 			
-			if (lastDateIndex != -1)
-				price += dp[lastDateIndex];
-			
-			minPrice = Math.Min(minPrice, price);
+			var tickets = _GenTickets(costs);
+			return _GetMinCost(days, tickets);
 		}
 
-		return minPrice;
-	}
+		private Ticket[] _GenTickets(int[] costs)
+			=> new Ticket[]
+			{
+				new Ticket
+				{
+					Cost = costs[0],
+					Period = 1,
+				},
+				new Ticket
+				{
+					Cost = costs[1],
+					Period = 7,
+				},
+				new Ticket
+				{
+					Cost = costs[2],
+					Period = 30,
+				},
+			};
 
-	private int _GetLastDateIndex(int date, int[] days) {
-		var lastIndex = -1;
-		for (int i = 0; i < days.Length; ++i) {
-			if (days[i] > date) return lastIndex;
-			else lastIndex = i;
+		private int _GetMinCost(int[] days, Ticket[] tickets)
+		{
+			var table = new int[days.Length];
+
+			for (int i = 0; i < table.Length; ++i)
+			{
+				var minCost = int.MaxValue;
+				foreach (var ticket in tickets)
+				{
+					var cost = ticket.Cost;
+					for (int j = i-1; j >= 0; --j)
+						if (days[j] <= days[i] - ticket.Period)
+						{
+							cost += table[j];
+							break;
+						}
+					minCost = Math.Min(minCost, cost);
+				}
+				table[i] = minCost;
+			}
+
+			return table.Last();
 		}
-		return lastIndex;
 	}
 }
