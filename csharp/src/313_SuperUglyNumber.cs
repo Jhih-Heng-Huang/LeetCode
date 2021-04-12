645bf19b62d188cf69348dbfa7ff7833f59b504e
Jhih-Heng-Huang <kklive128@gmail.com>
2021-04-13 06:36:23 +0800

    Solve LeetCode: 313. Super Ugly Number in C#

--- csharp/src/313_SuperUglyNumber.cs
+++ csharp/src/313_SuperUglyNumber.cs
@@ -1,38 +1,40 @@
 /*
-313. Super Ugly Number
+LeetCode: 313. Super Ugly Number
 */
 
 using System.Linq;
 
-public class SuperUglyNumber {
-	private class PrimePointer {
-		public int Value;
-		public int Position;
-	}
-
-    public int NthSuperUglyNumber(int n, int[] primes) {
-		if (n == 1) return 1;
-		
-		var primePointers = primes.Select(prime =>
-			new PrimePointer{Value = prime, Position = 0})
-			.ToArray();
-		
-		return _FindNthSuperUglyNumber(n, primePointers);
-    }
-
-	private int _FindNthSuperUglyNumber(int n, PrimePointer[] primePointers) {
-		var table = new int[n];
-		table[0] = 1;
-
-		for (int i = 1; i < n; i++) {
-			table[i] = primePointers.Min(primePointer => primePointer.Value * table[primePointer.Position]);
-
-			foreach (var primePointer in primePointers) {
-				if (table[i] == primePointer.Value * table[primePointer.Position])
-					++primePointer.Position;
-			}
+namespace LeetCode.Problem_313
+{
+	public class SuperUglyNumber {
+		private class PrimePointer
+		{
+			public int Value;
+			public int Index;
 		}
 
-		return table[n-1];
+		public int NthSuperUglyNumber(int n, int[] primes) {
+			if (n <= 1) return 1;
+
+			var primePointers = primes.Select(prime => new PrimePointer{ Value = prime, Index = 0})
+				.ToArray();
+			return _FindNthSuperUglyNumber(n, primePointers);
+		}
+
+		private int _FindNthSuperUglyNumber(int n, PrimePointer[] primePointers)
+		{
+			var table = new int[n];
+			table[0] = 1;
+
+			for (int i = 1; i < table.Length; ++i)
+			{
+				table[i] = primePointers.Min(pointer => pointer.Value * table[pointer.Index]);
+
+				foreach (var pointer in primePointers)
+					if (table[i] == pointer.Value * table[pointer.Index])
+						++pointer.Index;
+			}
+			return table[n-1];
+		}
 	}
 }
