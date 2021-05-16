8b7081e8d791990fd4d01f3882fdba5482894826
Jhih-Heng-Huang <kklive128@gmail.com>
2021-05-16 16:01:09 +0800

    Solve LeetCode: 20. Valid Parentheses in C#

--- csharp/src/20_ValidParentheses.cs
+++ csharp/src/20_ValidParentheses.cs
@@ -13,7 +13,7 @@ namespace LeetCode.Problem_20
 
 			foreach (var c in s)
 			{
-				if (_IsLeftParenthese(c))
+				if (_IsLeftParenthesis(c))
 					stack.Push(c);
 				else if (stack.Count != 0 &&
 					_IsPaired(stack.Peek(), c))
@@ -25,13 +25,15 @@ namespace LeetCode.Problem_20
 			return stack.Count == 0;
 		}
 
-		private bool _IsLeftParenthese(char c)
+		private bool _IsLeftParenthesis(char c)
 		{
-			return c == '(' || c == '[' || c == '{';
+			return c == '[' || c == '(' || c == '{';
 		}
 
-		private bool _IsPaired(char left, char right) {
-			return (left == '(' && right == ')') ||
+		private bool _IsPaired(char left, char right)
+		{
+			return
+				(left == '(' && right == ')') ||
 				(left == '[' && right == ']') ||
 				(left == '{' && right == '}');
 		}
