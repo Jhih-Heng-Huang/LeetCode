f18ee69662fe1a997c84d877d3024f5849cf760c
Jhih-Heng-Huang <kklive128@gmail.com>
2021-06-26 06:36:26 +0800

    Solve LeetCode: 22. Generate Parentheses in C#

--- csharp/src/22_GenerateParentheses.cs
+++ csharp/src/22_GenerateParentheses.cs
@@ -10,24 +10,23 @@ namespace LeetCode.Problem_22
 	{
 		public IList<string> GenerateParenthesis(int n)
 		{
-			var list = new List<string>();
-			_GenParenthesis(list, string.Empty, n, 0);
-			return list;
+			var results = new List<string>();
+			_GenParenthesis(results, string.Empty, n, 0);
+			return results;
 		}
 
-		private void _GenParenthesis(IList<string> list, string result,
+		private void _GenParenthesis(
+			IList<string> results, string result,
 			int leftNum, int rightNum)
 		{
 			if (leftNum == 0 && rightNum == 0)
 			{
-				list.Add(result);
+				results.Add(result);
 				return;
 			}
 
-			if (leftNum > 0)
-				_GenParenthesis(list, result+"(", leftNum-1, rightNum+1);
-			if (rightNum > 0)
-				_GenParenthesis(list, result+")", leftNum, rightNum-1);
+			if (leftNum > 0) _GenParenthesis(results, result+"(", leftNum-1, rightNum+1);
+			if (rightNum > 0) _GenParenthesis(results, result+")", leftNum, rightNum-1);
 		}
 	}
 }
