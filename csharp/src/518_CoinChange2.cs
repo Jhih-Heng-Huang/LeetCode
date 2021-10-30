10783a2c29b44b9c4f2e0eca02de83e386775340
Jhih-Heng-Huang <kklive128@gmail.com>
2021-10-30 16:04:54 +0800

    Solve LeetCode: 518. Coin Change 2

--- csharp/src/518_CoinChange2.cs
+++ csharp/src/518_CoinChange2.cs
@@ -8,22 +8,20 @@ namespace LeetCode.Problem_518
 {
 	public class CoinChange2 {
 		public int Change(int amount, int[] coins) {
-			if (amount <= 0) return 1;
+			if (amount == 0) return 1;
 
-			var table = _GenTable(amount);
+			var table = _InitTable(amount);
 
 			foreach (var coin in coins)
-				for (int i = coin; i <= amount; ++i)
+				for (int i = coin; i < table.Length; ++i)
 					table[i] += table[i-coin];
 
 			return table[amount];
 		}
 
-		private int[] _GenTable(int amount) {
-			var table = new int[amount + 1];
-
-			for (int i = 0; i <= amount; ++i)
-				table[i] = 0;
+		private int[] _InitTable(int amount)
+		{
+			var table = new int[amount+1];
 			table[0] = 1;
 			return table;
 		}
