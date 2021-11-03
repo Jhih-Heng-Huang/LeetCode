4b30392fe51d1657101d205c02d1510a2e494f26
Albert Huang <Jhih-Heng-Huang@github.com>
2021-11-03 10:23:59 +0800

    Solve LeetCode: 1277. Count Square Submatrices with All Ones in C#

--- csharp/src/1277_CountSquareSubmatriceswithAllOnes.cs
+++ csharp/src/1277_CountSquareSubmatriceswithAllOnes.cs
@@ -2,54 +2,37 @@
 LeetCode: 1277. Count Square Submatrices with All Ones
 */
 
-using System;
 using System.Linq;
 
 public class CountSquareSubmatriceswithAllOnes {
     public int CountSquares(int[][] matrix) {
-		if (matrix == null ||
-			matrix.Length == 0 ||
-			matrix[0].Length == 0)
-			return 0;
-		
-		return _CountSquares(matrix);
-    }
-
-	private int _CountSquares(int[][] matrix)
-	{
 		var table = _GenTable(matrix.Length, matrix[0].Length);
-
-		for (int row = 0; row < matrix.Length; ++row)
-			for (int col = 0; col < matrix[row].Length; ++col)
+		var count = 0;
+		for (int row = 0; row < table.Length; ++row)
+			for (int col = 0; col < table[row].Length; ++col)
 			{
+				if (matrix[row][col] == 0) continue;
 				if (row == 0 || col == 0)
 					table[row][col] = matrix[row][col];
-				else if (matrix[row][col] == 0)
-					table[row][col] = 0;
 				else
-					table[row][col] = 1 + new int[] {
+					table[row][col] = new int[]
+					{
 						table[row-1][col],
+						table[row-1][col-1],
 						table[row][col-1],
-						table[row-1][col-1]}.Min();
+					}.Min() + 1;
+				count += table[row][col];
+
 			}
-		
-		var count = 0;
-		foreach (var row in table)
-			foreach (var ele in row)
-				count += ele;
-		
 		return count;
-	}
+    }
 
-	private int[][] _GenTable(int numRows, int numCols) {
-		var table = new int[numRows][];
+	private int[][] _GenTable(int height, int width)
+	{
+		var table = new int[height][];
 
-		for (int row = 0; row < numRows; ++row) {
-			table[row] = new int[numCols];
-			for (int col = 0; col < numCols; ++col)
-				table[row][col] = 0;
-		}
-
+		for (int i = 0; i < height; ++i)
+			table[i] = new int[width];
 		return table;
 	}
 }
