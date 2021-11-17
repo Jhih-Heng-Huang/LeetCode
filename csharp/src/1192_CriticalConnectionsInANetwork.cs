78493d9ca2cbe4d052b8dda44d958d736c4395c7
Albert Huang <Jhih-Heng-Huang@github.com>
2021-11-17 10:21:11 +0800

    Solve LeetCode 1192. Critical Connections in a Network in C#

--- csharp/src/1192_CriticalConnectionsInANetwork.cs
+++ csharp/src/1192_CriticalConnectionsInANetwork.cs
@@ -7,70 +7,62 @@ using System.Collections.Generic;
 using System.Linq;
 
 public class CriticalConnectionsInANetwork {
+	private const int NON_VISITED = -2;
 	private class Node {
-		public int Rank = -2;
-		public List<int> Nexts = new List<int>();
+		public int rank = NON_VISITED;
+		public List<int> nexts = new List<int>();
 	}
+
 
 	public IList<IList<int>> CriticalConnections(
 		int n, IList<IList<int>> connections)
 	{
-		if (n == 0 || connections == null ||
-			connections.Count == 0)
-			return new List<IList<int>>();
-
 		var nodes = _GenGraph(n, connections);
-		var criticalEdges = new List<IList<int>>();
 
+		var edges = new List<IList<int>>();
 		for (int i = 0; i < nodes.Length; ++i)
-			if (nodes[i].Rank == -2)
-				_GetMinRank(i, 0, nodes, criticalEdges);
-
-		return criticalEdges;
+			if (nodes[i].rank == NON_VISITED)
+				_GetRank(-1, i, 0, nodes, edges);
+		return edges;
 	}
+
 
 	private Node[] _GenGraph(int n, IList<IList<int>> edges)
 	{
 		var nodes = new Node[n];
-
-		for (int i = 0; i < nodes.Length; ++i)
+		for (int i = 0; i < n; ++i)
 			nodes[i] = new Node();
-
-		foreach (var edge in edges) {
+		foreach (var edge in edges)
+		{
 			var i = edge[0];
 			var j = edge[1];
-			nodes[i].Nexts.Add(j);
-			nodes[j].Nexts.Add(i);
+			nodes[i].nexts.Add(j);
+			nodes[j].nexts.Add(i);
 		}
-
 		return nodes;
 	}
-
-	private int _GetMinRank(int i, int rank, Node[] nodes, IList<IList<int>> criticalEdges)
+	private int _GetRank(
+		int last,
+		int current,
+		int assignRank,
+		Node[] nodes,
+		IList<IList<int>> edges)
 	{
-		if (nodes[i].Rank != -2) return nodes[i].Rank;
+		nodes[current].rank = assignRank;
 
-		nodes[i].Rank = rank;
-		var minRank = rank;
-		var num = nodes.Length;
-
-		foreach (var next in nodes[i].Nexts)
+		foreach (var next in nodes[current].nexts)
 		{
-			if (nodes[next].Rank == rank-1 ||
-				nodes[next].Rank == num)
-				continue;
-			
-			var nextRank = _GetMinRank(next, rank + 1,
-				nodes, criticalEdges);
-			
-			if (rank >= nextRank)
-				minRank = Math.Min(minRank, nextRank);
-			else {
-				criticalEdges.Add(new int[]{i, next}.ToList());
-			}
-		}
+			if (next == last) continue;
 
-		nodes[i].Rank = num;
-		return minRank;
+			var nextRank = (nodes[next].rank != NON_VISITED)?
+				nodes[next].rank : _GetRank(current, next, assignRank+1, nodes, edges);
+			
+			if (nextRank <= assignRank)
+				nodes[current].rank = Math.Min(nodes[current].rank, nextRank);
+			else
+				edges.Add(new int[]{current, next}.ToList());
+		}
+		return nodes[current].rank;
 	}
+
 }
