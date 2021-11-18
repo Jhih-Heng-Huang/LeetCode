6f4ad732e5285af6fd3233da8bc1415d6b087fce
Jhih-Heng-Huang <kklive128@gmail.com>
2021-11-19 06:13:25 +0800

    Solve LeetCode: 986. Interval List Intersections in C#

--- csharp/src/986_IntervalListIntersections.cs
+++ csharp/src/986_IntervalListIntersections.cs
@@ -7,40 +7,34 @@ using System.Collections.Generic;
 using System.Linq;
 
 public class IntervalListIntersections {
-	private class Interval {
-		public int Start;
-		public int End;
-	}
-    public int[][] IntervalIntersection(int[][] A, int[][] B) {
-		List<int[]> results = new List<int[]>();
+	private const int left = 0;
+	private const int right = 1;
 
-		if (A == null || A.Length == 0 ||
-			B == null || B.Length == 0)
-			return results.ToArray();
+    public int[][] IntervalIntersection(int[][] A, int[][] B)
+	{
+		if (A.Length == 0 ||
+			B.Length == 0)
+			return new int[][]{};
+		
+		var intervals = new List<int[]>();
 
-		var list1 = A.Select(a => new Interval{Start = a[0], End = a[1]}).ToArray();
-		var list2 = B.Select(b => new Interval{Start = b[0], End = b[1]}).ToArray();
-		var i = 0;
-		var j = 0;
-		while (i < list1.Length && j < list2.Length) {
-			var interval = _Intersect(list1[i], list2[j]);
+		int intervalA = 0;
+		int intervalB = 0;
+		while (intervalA < A.Length && intervalB < B.Length)
+		{
+			var leftBound = Math.Max(A[intervalA][left], B[intervalB][left]);
+			var rightBound = Math.Min(A[intervalA][right], B[intervalB][right]);
+			// get intersection
+			if (leftBound <= rightBound)
+				intervals.Add(new int[]{leftBound, rightBound});
 
-			if (interval != null)
-				results.Add(new int[] {interval.Start, interval.End});
-			
-			if (list1[i].End <= list2[j].End) ++i;
-			else ++j;
+			// go next
+			if (A[intervalA][right] <= B[intervalB][right])
+				++intervalA;
+			else
+				++intervalB;
 		}
 
-		return results.ToArray();
+		return intervals.ToArray();
     }
-
-	private Interval _Intersect(Interval interval1, Interval interval2)
-	{
-		var left = Math.Max(interval1.Start, interval2.Start);
-		var right = Math.Min(interval1.End, interval2.End);
-		if (left > right) return null;
-		return new Interval{Start = left, End = right};
-	}
-
 }
