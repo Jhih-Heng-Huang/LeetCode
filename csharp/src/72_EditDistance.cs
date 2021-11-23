497f962b6ec33f43f15773378a466faa969bf57c
Albert Huang <Jhih-Heng-Huang@github.com>
2021-11-23 10:32:01 +0800

    Solve LeetCode 72. Edit Distance in C#

--- csharp/src/72_EditDistance.cs
+++ csharp/src/72_EditDistance.cs
@@ -2,49 +2,68 @@
 LeetCode 72. Edit Distance
 */
 
-using System;
+using System.Linq;
 
 namespace LeetCode.Problem_72
 {
 	public class EditDistance {
+		private const int NAN = -1;
 		public int MinDistance(string word1, string word2)
 		{
-			if (string.IsNullOrEmpty(word1) &&
-				string.IsNullOrEmpty(word2)) return 0;
-			if (string.IsNullOrEmpty(word1)) return word2.Length;
-			if (string.IsNullOrEmpty(word2)) return word1.Length;
+			if (word1.Length == 0) return word2.Length;
+			if (word2.Length == 0) return word1.Length;
 
-			return _GetMinEditDistance(word1, word2);
+			var table = _GenTable(word1.Length, word2.Length);
+			return _FindMinDistance(
+				word1,
+				word2,
+				word1.Length,
+				word2.Length,
+				table);
 		}
 
-		private int _GetMinEditDistance(string word1, string word2)
+		private int[][] _GenTable(
+			int str1Length,
+			int str2Length)
 		{
-			var table = _GenTable(
-				rowNum: word1.Length + 1,
-				colNum: word2.Length + 1);
-
+			var table = new int[str1Length+1][];
 			for (int row = 0; row < table.Length; ++row)
+			{
+				table[row] = new int[str2Length+1];
 				for (int col = 0; col < table[row].Length; ++col)
-					if (row == 0)
-						table[row][col] = col;
-					else if (col == 0)
-						table[row][col] = row;
-					else if (word1[row-1] == word2[col-1])
-						table[row][col] = table[row-1][col-1];
-					else
-						table[row][col] = 1 + Math.Min(table[row-1][col-1],
-							Math.Min(table[row-1][col], table[row][col-1]));
-			return table[word1.Length][word2.Length];
-		}
-
-		private int[][] _GenTable(int rowNum, int colNum)
-		{
-			var table = new int[rowNum][];
+					table[row][col] = NAN;
+			}
 
 			for (int row = 0; row < table.Length; ++row)
-				table[row] = new int[colNum];
-			
+				table[row][0] = row;
+			for (int col = 0; col < table[0].Length; ++col)
+				table[0][col] = col;
 			return table;
+		}
+
+		private int _FindMinDistance(
+			string str1,
+			string str2,
+			int str1Length,
+			int str2Length,
+			int[][] table)
+		{
+			if (table[str1Length][str2Length] != NAN)
+				return table[str1Length][str2Length];
+
+			var word1 = str1[str1Length-1];
+			var word2 = str2[str2Length-1];
+
+			if (word1 == word2)
+				table[str1Length][str2Length] = _FindMinDistance(str1, str2, str1Length-1, str2Length-1, table);
+			else
+				table[str1Length][str2Length] = new int[]
+				{
+					1 + _FindMinDistance(str1, str2, str1Length-1, str2Length, table),
+					1 + _FindMinDistance(str1, str2, str1Length, str2Length-1, table),
+					1 + _FindMinDistance(str1, str2, str1Length-1, str2Length-1, table),
+				}.Min();
+			return table[str1Length][str2Length];
 		}
 	}
 }
