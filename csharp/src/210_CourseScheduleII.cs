48c4211ab49c4044991820b53e59e230a6106dab
Albert Huang <Jhih-Heng-Huang@github.com>
2021-12-07 13:29:27 +0800

    Solve LeetCode: 210. Course Schedule II in C#

--- csharp/src/210_CourseScheduleII.cs
+++ csharp/src/210_CourseScheduleII.cs
@@ -10,76 +10,62 @@ namespace LeetCode.Problem_210
 	public class CourseScheduleII {
 		private enum State
 		{
-			NonVisited,
+			Unvisited,
 			Visiting,
 			Visited,
 		}
-
-		private class Course
+		private class Node
 		{
-			public State State = State.NonVisited;
-			public List<int> Requires = new List<int>();
+			public State state = State.Unvisited;
+			public List<int> nexts = new List<int>();
 		}
 
-		public int[] FindOrder(int numCourses,
+		public int[] FindOrder(
+			int numCourses,
 			int[][] prerequisites)
 		{
-			if (numCourses == 0)
-				return new int[]{};
-			
-			var courses = _GenCourses(numCourses, prerequisites);
 			var orders = new List<int>();
 
-			for (int i = 0; i < courses.Length; ++i)
-			{
-				var subOrders = new List<int>();
-				if (_IsNotVisited(courses[i]) &&
-					!_TryToTravel(i, courses, subOrders))
+			var nodes = _GenNodes(numCourses, prerequisites);
+
+			for (int i = 0; i < nodes.Length; ++i)
+				if (nodes[i].state == State.Unvisited && _HasCycle(i, nodes, orders))
 					return new int[]{};
-				orders.AddRange(subOrders);
-			}
 
 			return orders.ToArray();
 		}
 
-		private Course[] _GenCourses(int num, int[][] edges)
+		private Node[] _GenNodes(int numNodes, int[][] edges)
 		{
-			var courses = new Course[num];
-			for (int i = 0; i < num; ++i)
-				courses[i] = new Course();
-
+			var nodes = new Node[numNodes];
+			for (int i = 0; i < nodes.Length; ++i)
+				nodes[i] = new Node();
 			foreach (var edge in edges)
 			{
 				var i = edge[0];
 				var j = edge[1];
-				courses[i].Requires.Add(j);
+				nodes[i].nexts.Add(j);
 			}
-
-			return courses;
+			return nodes;
 		}
 
-		private bool _IsNotVisited(Course course)
-			=> course.State == State.NonVisited;
-
-		private bool _IsVisiting(Course course)
+		private bool _HasCycle(int current, Node[] nodes, List<int> orders)
 		{
-			return course.State == State.Visiting;
-		}
+			nodes[current].state = State.Visiting;
 
-		private bool _TryToTravel(int i, Course[] courses, List<int> orders)
-		{
-			if (_IsVisiting(courses[i])) return false;
-			if (!_IsNotVisited(courses[i])) return true;
-
-			courses[i].State = State.Visiting;
-			foreach (var next in courses[i].Requires)
+			foreach (var next in nodes[current].nexts)
 			{
-				if (!_TryToTravel(next, courses, orders))
-					return false;
+				if (nodes[next].state == State.Visited)
+					continue;
+				if (nodes[next].state == State.Visiting)
+					return true;
+				if (nodes[next].state == State.Unvisited &&
+					_HasCycle(next, nodes, orders))
+					return true;
 			}
-			courses[i].State = State.Visited;
-			orders.Add(i);
-			return true;
+			nodes[current].state = State.Visited;
+			orders.Add(current);
+			return false;
 		}
 	}
 }
