b06690df9073699955c9d43b075883539c7e48ef
Albert Huang <Jhih-Heng-Huang@github.com>
2021-12-22 16:58:08 +0800

    Solve LeetCode: 313. Super Ugly Number in C#

--- csharp/src/313_SuperUglyNumber.cs
+++ csharp/src/313_SuperUglyNumber.cs
@@ -2,6 +2,7 @@
 LeetCode: 313. Super Ugly Number
 */
 
+using System;
 using System.Linq;
 
 namespace LeetCode.Problem_313
@@ -9,30 +10,29 @@ namespace LeetCode.Problem_313
 	public class SuperUglyNumber {
 		private class PrimePointer
 		{
-			public int Value;
-			public int Index;
+			public int Val;
+			public int Pos = 0;
 		}
 
 		public int NthSuperUglyNumber(int n, int[] primes) {
-			if (n <= 1) return 1;
+			if (n == 1) return 1;
 
-			var primePointers = primes.Select(prime => new PrimePointer{ Value = prime, Index = 0})
-				.ToArray();
-			return _FindNthSuperUglyNumber(n, primePointers);
-		}
-
-		private int _FindNthSuperUglyNumber(int n, PrimePointer[] primePointers)
-		{
 			var table = new int[n];
 			table[0] = 1;
 
-			for (int i = 1; i < table.Length; ++i)
+			var primePointers = primes
+				.Select(prime => new PrimePointer{Val = prime})
+				.ToArray();
+			
+			for (int i = 1; i < n; ++i)
 			{
-				table[i] = primePointers.Min(pointer => pointer.Value * table[pointer.Index]);
-
-				foreach (var pointer in primePointers)
-					if (table[i] == pointer.Value * table[pointer.Index])
-						++pointer.Index;
+				var minVal = int.MaxValue;
+				foreach(var primePointer in primePointers)
+					minVal = Math.Min(minVal, primePointer.Val * table[primePointer.Pos]);
+				foreach(var primePointer in primePointers)
+					if (primePointer.Val * table[primePointer.Pos] == minVal)
+						++primePointer.Pos;
+				table[i] = minVal;
 			}
 			return table[n-1];
 		}
