73859e3018f8f98ba28b0d3058cbbddd0e1de4f1
Jhih-Heng-Huang <kklive128@gmail.com>
2021-12-26 11:02:03 +0800

    Solve LeetCode: 5. Longest Palindromic Substring in C#

--- csharp/src/5_LongestPalindromicSubstring.cs
+++ csharp/src/5_LongestPalindromicSubstring.cs
@@ -6,39 +6,38 @@ namespace LeetCode.Problem_5
 {
 	public class LongestPalindromicSubstring {
 		public string LongestPalindrome(string s) {
-			if (s == null || s.Length == 0)
-				return string.Empty;
-			if (s.Length == 1)
-				return s;
-			
+			int maxLen = 0;
 			var result = string.Empty;
 			for (int i = 0; i < s.Length; ++i)
 			{
-				var str1 = _FindLongestPalindrome(s, i-1, i);
-				if (str1.Length > result.Length)
-					result = str1;
-				
-				var str2 = _FindLongestPalindrome(s, i, i);
-				if (str2.Length > result.Length)
-					result = str2;
+				var subStr1 = _FindLongestPalindrome(s, i, i);
+				if (maxLen < subStr1.Length)
+				{
+					maxLen = subStr1.Length;
+					result = subStr1;
+				}
+				var subStr2 = _FindLongestPalindrome(s, i, i+1);
+				if (maxLen < subStr2.Length)
+				{
+					maxLen = subStr2.Length;
+					result = subStr2;
+				}
 			}
-
 			return result;
 		}
 
-		private string _FindLongestPalindrome(string s, int left, int right)
+		private string _FindLongestPalindrome(string s, int leftIdx, int rightIdx)
 		{
-			while (left >= 0 && right < s.Length && s[left] == s[right])
+			while (leftIdx >= 0 && rightIdx < s.Length)
 			{
-				--left;
-				++right;
+				if (s[leftIdx] != s[rightIdx]) break;
+				--leftIdx;
+				++rightIdx;
 			}
-			++left;
-			--right;
-			
-			if (left > right)
-				return string.Empty;
-			return s.Substring(left, right - left + 1);
+
+			++leftIdx;
+			--rightIdx;
+			return s.Substring(leftIdx, rightIdx - leftIdx + 1);			
 		}
 	}
 }
